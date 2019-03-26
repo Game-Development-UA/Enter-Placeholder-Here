40559452922d373e114c6e826c99b1002d625a13
Catie Baker <bakerc2@email.arizona.edu>
2019-03-26 10:45:41 -0700

    The REAL game files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    float hor;
+    float vert;
+    public Rigidbody body;
+
+    private void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        vert = Input.GetAxis("Vertical");
+        body.velocity = (new Vector2(hor * speed, vert * speed));
+    }
+}
