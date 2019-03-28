519ba7271dea3332b240fe21246bc16f1c192c3a
Catie Baker <bakerc2@email.arizona.edu>
2019-03-28 10:44:14 -0700

    Correction to PlayerController
    Changed Button Input. Vert=Space
    Z=up,down,w,s

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,18 @@ public class PlayerController : MonoBehaviour
     public float speed;
     float hor;
     float vert;
+    float depth;
     public Rigidbody body;
 
     private void Update()
     {
         hor = Input.GetAxis("Horizontal");
         vert = Input.GetAxis("Vertical");
-        body.velocity = (new Vector2(hor * speed, vert * speed));
+        depth = Input.GetAxis("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        body.velocity = (new Vector3(hor * speed, vert * speed, depth * speed));
     }
 }
