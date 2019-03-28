c33f40f8deaa1ba097a67af7870c3590a6d8e0af
Catie Baker <bakerc2@email.arizona.edu>
2019-03-28 10:54:56 -0700

    New Chunk Script

---
+++ Assets/Scripts/LoadChunkLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadChunkLevel : MonoBehaviour
+{
+    public Transform nextChunkLoc;
+    public GameObject curChunk;
+    public GameObject nextChunk;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        PlayerController player = col.GetComponent<PlayerController>();
+
+        if (player)
+        {
+            GameObject newChunk = Instantiate<GameObject>(nextChunk);
+            newChunk.transform.position = nextChunkLoc.position;
+
+            Destroy(curChunk, 10f);
+        }
+    }
+}
