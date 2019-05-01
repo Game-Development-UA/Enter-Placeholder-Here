cbd9786d40d74c0acac9dfd405f5e35a9e8296a0
Catie Baker <bakerc2@email.arizona.edu>
2019-04-30 19:59:34 -0700

    Added Timer to End Game
    Player now has 30 secs to get as far as possible

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    float timeLeft = 30.0f;
+    public GameObject player;
+    public TextMeshProUGUI timer;
+
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("Game Over");
+        }
+        SetTimer();
+    }
+
+    public void SetTimer()
+    {
+        timer.text = timeLeft.ToString();
+    }
+}
