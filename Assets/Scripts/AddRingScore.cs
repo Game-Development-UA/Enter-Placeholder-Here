08aa2654b5fde8df3d928f51a73cc9dfd01b3414
Catie Baker <bakerc2@email.arizona.edu>
2019-04-30 19:28:49 -0700

    Score script and ring triggers added
    Also TextMeshPro. It's not as scary as it looks

---
+++ Assets/Scripts/AddRingScore.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRingScore : MonoBehaviour
+{
+    public GameObject ring;
+    public GameObject ring1;
+    public GameObject ring2;
+    public GameObject ring3;
+    public GameObject ring4;
+    public GameObject ring5;
+
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+            this.transform.parent = col.transform;
+            Score.singleton.SetScore(10);
+    }
+
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; 
+
+
+public class Score : MonoBehaviour
+{
+    public float score;
+    public TextMeshProUGUI ringScore;
+
+    public static Score singleton;
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+
+    private void Start()
+    {
+        score = 0;
+    }
+
+    public void SetScore(int numToAdd)
+    {
+        score = score + numToAdd;
+        ringScore.text = score.ToString();
+    }
+}
