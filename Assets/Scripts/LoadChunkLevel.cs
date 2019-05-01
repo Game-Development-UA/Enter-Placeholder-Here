80b6f894f11ae5b24f22f43b0e23abc91b3e2b7f
Catie Baker <bakerc2@email.arizona.edu>
2019-04-30 18:32:28 -0700

    Created Prefab Varients
    Adjusted Script to load random level chunk

--- Assets/Scripts/LoadChunkLevel.cs
+++ Assets/Scripts/LoadChunkLevel.cs
@@ -6,15 +6,33 @@ public class LoadChunkLevel : MonoBehaviour
 {
     public Transform nextChunkLoc;
     public GameObject curChunk;
-    public GameObject nextChunk;
+    //public GameObject nextChunk;
+    public GameObject Prefab1;
+    public GameObject Prefab2;
+    public GameObject Prefab3;
+     
 
-    private void OnTriggerEnter(Collider col)
+    List<GameObject> prefabList = new List<GameObject>();
+
+    void Start()
     {
+        prefabList.Add(Prefab1);
+        prefabList.Add(Prefab2);
+        prefabList.Add(Prefab3);
+
+        
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        int prefabIndex = UnityEngine.Random.Range(0, 3);
+
         PlayerController player = col.GetComponent<PlayerController>();
 
         if (player)
         {
-            GameObject newChunk = Instantiate<GameObject>(nextChunk);
+            GameObject newChunk = Instantiate(prefabList[prefabIndex]); //Instantiate<GameObject>(nextChunk);
             newChunk.transform.position = nextChunkLoc.position;
 
             Destroy(curChunk, 5f);
